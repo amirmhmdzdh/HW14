d35af59dd76009d0b74942544e1b8c4f311e5a3c
amirmhmdzdh <amir0mohammadzadeh@gmail.com>
2024-03-13 02:14:27 +0330

    add move_task_to_done , delete_task , search_tasks , main_menu

--- src/bash/bash.sh
+++ src/bash/bash.sh
@@ -38,3 +38,103 @@ function remove_task() {
   sed -i "${task_number}d" "$database_file_undone"
   echo "Task removed: $task_number"
 }
+
+function move_task_to_done() {
+
+  task_number=$1
+  task=$(sed -n "${task_number}p" "$database_file_undone")
+
+  echo "$task" >>"$database_file_done"
+  sed -i "${task_number}d" "$database_file_undone"
+  echo "Task marked as done and moved to 'done.txt'"
+}
+
+function delete_task() {
+
+  task_number=$1
+  task=$(sed -n "${task_number}p" "$database_file_undone")
+
+  echo "$task" >>"$database_file_deleted"
+  sed -i "${task_number}d" "$database_file_undone"
+  echo "Task marked as done and moved to 'Deleted.txt'"
+
+}
+
+function search_tasks() {
+
+  read -p "Enter the search Phrase: " search_Phrase
+  echo "Search results:"
+  echo "--------------"
+
+  echo "Tasks not done:"
+  echo "--------------"
+  grep -n "$search_Phrase" "$database_file_undone"
+
+  echo "Tasks done:"
+  echo "--------------"
+  grep -n "$search_Phrase" "$database_file_done"
+
+  echo "Tasks deleted:"
+  echo "--------------"
+  grep -n "$search_Phrase" "$database_deleted"
+
+}
+function main_menu {
+  while true; do
+    echo ""
+    echo "Main Menu:"
+    echo "1. Show tasks not done"
+    echo "2. Show tasks done"
+    echo "3. Show tasks deleted"
+    echo "4. Add task"
+    echo "5. Remove task"
+    echo "6. Mark task as done and move to 'done.txt'"
+    echo "7. Move task to 'Deleted.txt'"
+    echo "8. Search tasks"
+    echo "9. Exit"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+    1)
+      show_tasks_undone
+      ;;
+    2)
+      show_tasks_done
+      ;;
+    3)
+      show_tasks_deleted
+      ;;
+    4)
+      read -p "Enter the task description: " task_description
+      add_task "$task_description"
+      ;;
+    5)
+      read -p "Enter the task number to remove: " task_number
+      remove_task "$task_number"
+      ;;
+    6)
+      read -p "Enter the task number to mark as done and move: " task_number
+      move_task_to_done "$task_number"
+      ;;
+    7)
+      read -p "Enter the task number to move to 'Deleted.txt': " task_number
+      move_task_to_deleted "$task_number"
+      ;;
+
+    8)
+      search_tasks
+      ;;
+
+    9)
+      echo "Goodbye!"
+      exit 0
+      ;;
+
+    *)
+      echo "Invalid choice"
+      ;;
+    esac
+  done
+}
+
+main_menu
